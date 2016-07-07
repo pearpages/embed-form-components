87bb1a2dbe8fb70ab9493432febd2262636146eb
pearpages <pere@soms.cat>
2016-07-07 15:03:45 +0200

    api service

---
+++ src/app/api.service.spec.ts
@@ -0,0 +1,18 @@
+/* tslint:disable:no-unused-variable */
+
+import {
+  beforeEach, beforeEachProviders,
+  describe, xdescribe,
+  expect, it, xit,
+  async, inject
+} from '@angular/core/testing';
+import { ApiService } from './api.service';
+
+describe('Apiservice Service', () => {
+  beforeEachProviders(() => [ApiService]);
+
+  it('should ...',
+      inject([ApiService], (service: ApiService) => {
+    expect(service).toBeTruthy();
+  }));
+});
---
+++ src/app/api.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+import { Ifieldset } from './ifieldset';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class ApiService {
+
+  constructor(private http: Http) { }
+
+  getLobs(): Observable<Ifieldset> {
+    return this.http.get('/api/lobs.json').map(response => response.json());
+  }
+
+  private handleError(error: any) {
+    // In a real world app, we might use a remote logging infrastructure
+    // We'd also dig deeper into the error to get a better message
+    let errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error(errMsg); // log to console instead
+    return Observable.throw(errMsg);
+  }
+
+}
