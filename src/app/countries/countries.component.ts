373280c7106370773a50e2b7397d9f1c728e54ba
pearpages <pere@soms.cat>
2016-07-11 08:50:27 +0200

    countries

--- src/app/countries/countries.component.ts
+++ src/app/countries/countries.component.ts
@@ -1,16 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CheckboxesComponent } from '../checkboxes/checkboxes.component';
+import { Ifieldset } from '../ifieldset';
+import { ApiService } from '../api.service';
+import { IFormelement } from '../iformelement';
 @Component({
   moduleId: module.id,
   selector: 'countries',
   templateUrl: 'countries.component.html',
-  styleUrls: ['countries.component.css']
+  styleUrls: ['countries.component.css'],
+  directives: [CheckboxesComponent]
 })
 export class CountriesComponent implements OnInit {
 
-  constructor() {}
+ @ViewChild(CheckboxesComponent) checkboxes: CheckboxesComponent;
+  data: Ifieldset;
+  name: string = 'countries';
+  title: string = 'Countries';
+
+  constructor(private apiService: ApiService) { }
 
   ngOnInit() {
+    this.apiService.getCountries(true)
+    .subscribe(
+      (data) => {this.data = data},
+      (error) => console.error(error),
+      () => 'put any debug comments here'
+    );
+  }
+
+  forceRefresh(cache) {
+    this.apiService.getCountries(cache)
+    .subscribe(
+      (data) => {this.checkboxes.forceRefresh = true; this.data = data},
+      (error) => console.error(error),
+      () => 'put any debug comments here'
+    );
   }
 
 }
