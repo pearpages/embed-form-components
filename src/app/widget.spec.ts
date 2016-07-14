009fc46e1aa5eafbf81dfc585ba94a5a87e1ba60
pearpages <pere@soms.cat>
2016-07-14 18:34:14 +0200

    abstract class widget

---
+++ src/app/widget.spec.ts
@@ -0,0 +1,16 @@
+/* tslint:disable:no-unused-variable */
+
+import {
+  beforeEach, beforeEachProviders,
+  describe, xdescribe,
+  expect, it, xit,
+  async, inject
+} from '@angular/core/testing';
+import {Widget} from './widget';
+
+describe('Widget', () => {
+  /*it('should create an instance', () => {
+    expect(new Widget()).toBeTruthy();
+  });
+  */
+});
---
+++ src/app/widget.ts
@@ -0,0 +1,26 @@
+import { OnInit } from '@angular/core';
+import { ApiService } from './api.service';
+import { FormService } from './form.service';
+import { Fieldset } from './fieldset';
+
+export abstract class Widget implements OnInit {
+    protected data: Fieldset;
+
+    constructor(protected apiService: ApiService, protected form: FormService,protected name:string,protected title:string,protected method: string) { }
+
+    ngOnInit() {
+        this.forceRefresh(true);
+        this.form.setValue(this.name, this.data);
+    }
+
+    forceRefresh(useCache: boolean = false): void {
+        this.apiService[this.method](useCache)
+            .subscribe(
+            (data) => { this.data = data },
+            (error) => console.error(error),
+            () => 'put any debug comments here'
+            );
+    }
+
+    abstract validate(): boolean;
+}
