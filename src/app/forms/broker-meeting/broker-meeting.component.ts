695bbe6f324e9a2a98349ec7980e0fdd6f823cd5
pearpages <pere@soms.cat>
2016-07-19 19:07:35 +0200

    mock forms

--- src/app/forms/broker-meeting/broker-meeting.component.ts
+++ src/app/forms/broker-meeting/broker-meeting.component.ts
@@ -3,12 +3,13 @@ import { FormService } from '../../services/form.service';
 import { OfficesComponent } from '../../form-elements/extended/offices/offices.component';
 import { RegionsSetComponent } from '../../form-elements/extended/regions-set/regions-set.component';
 import { RegionsComponent } from '../../form-elements/extended/regions/regions.component';
+import { CountriesComponent } from '../../form-elements/extended/countries/countries.component';
 
 @Component({
   moduleId: module.id,
   selector: 'app-broker-meeting',
   templateUrl: 'broker-meeting.component.html',
-  directives: [OfficesComponent,RegionsSetComponent,RegionsComponent],
+  directives: [OfficesComponent,RegionsSetComponent,RegionsComponent,CountriesComponent],
   providers: [FormService]
 })
 export class BrokerMeetingComponent implements OnInit {
