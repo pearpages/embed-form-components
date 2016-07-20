47587a645078d9e22ba5b2c0bc4ebff5d9354ac2
pearpages <pere@soms.cat>
2016-07-20 11:31:22 +0200

    forms added

--- src/app/forms/gross-written-premium/gross-written-premium.component.ts
+++ src/app/forms/gross-written-premium/gross-written-premium.component.ts
@@ -1,10 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { FormService } from '../../services/form.service';
+import { OfficesComponent } from '../../form-elements/extended/offices/offices.component';
+import { RegionsSetComponent } from '../../form-elements/extended/regions-set/regions-set.component';
+import { CountriesComponent } from '../../form-elements/extended/countries/countries.component';
+import { CompanytypeComponent } from '../../form-elements/extended/companytype/companytype.component';
 
 @Component({
   moduleId: module.id,
   selector: 'app-gross-written-premium',
   templateUrl: 'gross-written-premium.component.html',
-  styleUrls: ['gross-written-premium.component.css']
+  styleUrls: ['gross-written-premium.component.css'],
+  providers: [FormService],
+  directives: [OfficesComponent,RegionsSetComponent,CountriesComponent,CompanytypeComponent]
 })
 export class GrossWrittenPremiumComponent implements OnInit {
 
--- src/app/forms/limit-exception/limit-exception.component.ts
+++ src/app/forms/limit-exception/limit-exception.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { FormService } from '../../services/form.service'; 
+import { OfficesComponent } from '../../form-elements/extended/offices/offices.component';
 
 @Component({
   moduleId: module.id,
   selector: 'app-limit-exception',
   templateUrl: 'limit-exception.component.html',
-  styleUrls: ['limit-exception.component.css']
+  styleUrls: ['limit-exception.component.css'],
+  directives: [OfficesComponent],
+  providers: [FormService]
 })
 export class LimitExceptionComponent implements OnInit {
 
