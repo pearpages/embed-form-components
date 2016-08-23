90a7b9700978fbfa2fd92292a03364ea78de00be
pearpages <pere@soms.cat>
2016-08-23 18:33:03 +0200

    prod version

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -12,91 +12,91 @@ export class ApiService {
 
   getLobs(cache: boolean): Observable<Fieldset> {
     const id = 'form-lobs';
-    const url = '/api/lobs.json';
+    const url = 'api/lobs.json';
     return this.getFieldset(id, url, cache);
   }
 
   getCompanyTypes(cache: boolean): Observable<Fieldset> {
     const id = 'form-company-types';
-    const url = '/api/company-types.json';
+    const url = 'api/company-types.json';
     return this.getFieldset(id, url, cache);
   }
 
   getBusiness(cache:boolean): Observable<Fieldset> {
     const id = 'form-business';
-    const url = '/api/business.json';
+    const url = 'api/business.json';
     return this.getFieldset(id,url,cache);
   }
 
   getOperations(cache:boolean): Observable<Fieldset> {
     const id = 'form-operations';
-    const url = '/api/operations.json';
+    const url = 'api/operations.json';
     return this.getFieldset(id,url,cache);
   }
 
   getUSexposed(cache: boolean): Observable<Fieldset> {
     const id = 'form-us-exposed';
-    const url = '/api/us-exposed.json';
+    const url = 'api/us-exposed.json';
     return this.getFieldset(id, url, cache);
   }
 
   getOffices(cache: boolean): Observable<Fieldset> {
     const id = 'form-offices';
-    const url = '/api/offices.json';
+    const url = 'api/offices.json';
     return this.getFieldset(id, url, cache);
   }
   
   getTypesOfView(cache:boolean): Observable<Fieldset> {
     const id = 'form-types-of-view';
-    const url = '/api/types-of-view.json';
+    const url = 'api/types-of-view.json';
     return this.getFieldset(id,url,cache);
   }
 
   getPrimaryExcess(cache: boolean): Observable<Fieldset> {
     const id = 'form-primary-excess';
-    const url = '/api/primary-excess.json';
+    const url = 'api/primary-excess.json';
     return this.getFieldset(id, url, cache);
   }
 
   getMarketCo(cache:boolean): Observable<Fieldset> {
     const id = 'form-market-co';
-    const url = '/api/market-co.json';
+    const url = 'api/market-co.json';
     return this.getFieldset(id,url,cache);
   }
 
   getReportingFigures(cache: boolean): Observable<Fieldset> {
     const id = 'form-reporting-figures';
-    const url = '/api/reporting-figures.json';
+    const url = 'api/reporting-figures.json';
     return this.getFieldset(id, url, cache);
   }
 
   getUws(cache: boolean): Observable<Fieldset> {
     const id = 'form-uws';
-    const url = '/api/uws.json';
+    const url = 'api/uws.json';
     return this.getFieldset(id, url, cache);
   }
 
   getLocalPolicies(cache: boolean): Observable<Fieldset> {
     const id = 'form-local-policies';
-    const url = '/api/local-policies.json';
+    const url = 'api/local-policies.json';
     return this.getFieldset(id, url, cache);
   }
 
   getRegionsSets(cache: boolean): Observable<Fieldset> {
     const id = 'form-regions-sets';
-    const url = '/api/regions-sets.json';
+    const url = 'api/regions-sets.json';
     return this.getFieldset(id, url, cache);
   }
 
   getRegions(cache: boolean): Observable<Fieldset> {
     const id = 'form-regions';
-    const url = '/api/regions.json';
+    const url = 'api/regions.json';
     return this.getFieldset(id, url, cache);
   }
 
   getCountries(cache: boolean): Observable<Fieldset> {
     const id = 'form-countries';
-    const url = '/api/countries.json';
+    const url = 'api/countries.json';
     return this.getFieldset(id, url, cache);
   }
 
